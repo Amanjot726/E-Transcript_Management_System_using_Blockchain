3b0aa0ad8503accd7c32ee10bf375f8c362da7cc
Amanjot726 <67585828+Amanjot726@users.noreply.github.com>
2022-04-12 21:36:52 +0530

    Changes by dell

--- Main.py
+++ Main.py
@@ -21,11 +21,13 @@ class Blockchain:
         self.chain = []
         self.create_block(proof=1, previous_hash='0')
 
-    def create_block(self, proof, previous_hash):
+    def create_block(self, proof, previous_hash, dataHash=None):
         block = {'index': len(self.chain) + 1,
                  'timestamp': str(datetime.datetime.now()),
                  'proof': proof,
-                 'previous_hash': previous_hash}
+                 'previous_hash': previous_hash,
+                 'dataHash': dataHash
+                 }
         self.chain.append(block)
         return block
 
